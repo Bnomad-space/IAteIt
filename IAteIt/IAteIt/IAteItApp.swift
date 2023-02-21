217d0957f8257ccd843ee0a912ee22e29c147341
Rain <eunbkang@gmail.com>
2023-02-22 01:04:20 +0900

    Add Xcode Project

---
+++ IAteIt/IAteIt/IAteItApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAteItApp.swift
+//  IAteIt
+//
+//  Created by Eunbee Kang on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct IAteItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
