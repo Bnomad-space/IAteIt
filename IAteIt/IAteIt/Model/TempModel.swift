217e6d009bddc55c40caea4c994ea244c5362bb0
Rain <eunbkang@gmail.com>
2023-02-22 13:05:42 +0900

    [Add] foldering and temporary files

---
+++ IAteIt/IAteIt/Model/TempModel.swift
@@ -0,0 +1,8 @@
+//
+//  TempModel.swift
+//  IAteIt
+//
+//  Created by Eunbee Kang on 2023/02/22.
+//
+
+import Foundation
---
+++ IAteIt/IAteIt/View/MyProfile/MyProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  MyProfileView.swift
+//  IAteIt
+//
+//  Created by Eunbee Kang on 2023/02/22.
+//
+
+import SwiftUI
+
+struct MyProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyProfileView()
+    }
+}
