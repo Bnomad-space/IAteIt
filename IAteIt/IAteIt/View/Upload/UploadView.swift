976c980b648a899c5502fcc55932373e1f1a1b42
Willowwryu <tuyuyou@gmail.com>
2023-02-24 01:12:16 +0900

    [Feat] #2 셔터버튼과 전 후면 전환 버튼
    - ui구현

--- IAteIt/IAteIt/View/Upload/UploadView.swift
+++ IAteIt/IAteIt/View/Upload/UploadView.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct UploadView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray
+                .ignoresSafeArea(.all, edges: .all)
+
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Button(action: {}, label: {
+                        ZStack{
+                            Circle()
+                                .fill(Color.white)
+                                .frame(width: 55.33, height: 55.33, alignment: .center)
+                            Circle()
+                                .stroke(Color.white,lineWidth: 4)
+                                .frame(width: 72, height: 72, alignment: .center)
+                        }
+                        .padding(.leading,70)
+                    })
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                            .resizable()
+                            .frame(width: 48, height: 48)
+                            .foregroundColor(Color.black)
+                            .opacity(0.35)
+                            .padding(.trailing)
+                    })
+                    
+                }
+                
+            }
+        }
+        
     }
 }
+
 
 struct UploadView_Previews: PreviewProvider {
     static var previews: some View {
