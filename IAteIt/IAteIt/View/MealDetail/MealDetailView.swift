28851f77d81673e26bd5abcbaece80aade22d712
Rain <eunbkang@gmail.com>
2023-02-23 21:53:15 +0900

    [Feat] #1 loc and time on photocard

--- IAteIt/IAteIt/View/MealDetail/MealDetailView.swift
+++ IAteIt/IAteIt/View/MealDetail/MealDetailView.swift
@@ -8,46 +8,80 @@
 import SwiftUI
 
 struct MealDetailView: View {
+
+    let paddingLR: CGFloat = 16
+    let photoCorner: CGFloat = 20
+    
     var body: some View {
         
-        VStack {
+        ScrollView {
             
-            Text("맥모닝")
-                .font(.headline)
-            
-            Text("2 hours ago")
-                .font(.footnote)
-            
-            ZStack {
-                Image("Sample_McMorning")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 358, height: 358, alignment: .center)
-                    .clipped()
-                    .cornerRadius(20)
+            VStack {
                 
-                VStack {
-                    HStack {
-                        Text("맥도날드")
-                            .font(.footnote)
-                        Spacer()
-                        Text("07:40")
-                            .font(.footnote)
+                Text("맥모닝")
+                    .font(.headline)
+                    .padding(EdgeInsets(top: 8, leading: paddingLR, bottom: 1, trailing: paddingLR))
+                
+                Text("2 hours ago")
+                    .font(.footnote)
+                    .padding([.bottom], 8)
+                
+                GeometryReader { metrics in
+                    let photoWidth = metrics.size.width - paddingLR * 2
+                    
+                    ZStack {
+                        Image("Sample_McMorning")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: photoWidth, height: photoWidth)
+                            .clipped()
+                            .cornerRadius(photoCorner)
+                        
+                        VStack {
+                            HStack {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: photoCorner)
+                                        .frame(width: 88, height: 28)
+                                        .opacity(0.6)
+                                        
+                                    HStack(alignment: .center) {
+                                        Image(systemName: "location")
+                                            .font(.footnote)
+                                        Text("맥도날드")
+                                            .font(.footnote)
+                                    }
+                                    .foregroundColor(.white)
+                                }
+                                Spacer()
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: photoCorner)
+                                        .frame(width: 80, height: 28)
+                                        .opacity(0.6)
+                                    
+                                    HStack {
+                                        Image(systemName: "clock")
+                                            .font(.footnote)
+                                        Text("07:40")
+                                            .font(.footnote)
+                                    }
+                                    .foregroundColor(.white)
+                                }
+                            }
+                            .padding()
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Spacer()
+                                Image(systemName: "plus")
+                            }
+                            .padding()
+                        }
                         
                     }
-                    .padding()
-                    
-                    Spacer()
-                    
-                    HStack {
-                        Spacer()
-                        Image(systemName: "plus")
-                    }
-                    .padding()
+                    .padding([.leading, .trailing], paddingLR)
                 }
-                
             }
-            .frame(width: 358, height: 358)
         }
     }
 }
