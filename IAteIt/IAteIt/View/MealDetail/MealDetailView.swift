085d348cd7ac28aa25624aac026ad82bfeae857f
Rain <eunbkang@gmail.com>
2023-02-24 23:32:51 +0900

    [Feat] #1 add comment bar UI

--- IAteIt/IAteIt/View/MealDetail/MealDetailView.swift
+++ IAteIt/IAteIt/View/MealDetail/MealDetailView.swift
@@ -17,27 +17,27 @@ struct MealDetailView: View {
     let photoCorner: CGFloat = 20
     
     var body: some View {
-        ScrollView {
-            VStack {
-                
-                Text("맥모닝")
-                    .font(.headline)
-                    .padding(EdgeInsets(top: 8, leading: paddingLR, bottom: 1, trailing: paddingLR))
-                
-                Text("2 hours ago")
-                    .font(.footnote)
-                    .padding([.bottom], 8)
-                
-                PhotoCardView(tagLocation: $tagLocation, tagTime: $tagTime)
-                
-                VStack(alignment: .leading, spacing: 12) {
-                    ForEach(tempCommentList, id: \.self) { comment in
-                        CommentView(comment: comment)
-                    }
+        ZStack {
+            ScrollView {
+                VStack {
+                    Text("맥모닝")
+                        .font(.headline)
+                        .padding(EdgeInsets(top: 8, leading: paddingLR, bottom: 1, trailing: paddingLR))
+                    Text("2 hours ago")
+                        .font(.footnote)
+                        .padding([.bottom], 8)
                     
+                    PhotoCardView(tagLocation: $tagLocation, tagTime: $tagTime)
+                    
+                    VStack(alignment: .leading, spacing: 12) {
+                        ForEach(tempCommentList, id: \.self) { comment in
+                            CommentView(comment: comment)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 24, leading: paddingLR, bottom: 0, trailing: paddingLR))
                 }
-                .padding(EdgeInsets(top: 24, leading: paddingLR, bottom: 0, trailing: paddingLR))
             }
+            AddCommentBarView()
         }
     }
 }
