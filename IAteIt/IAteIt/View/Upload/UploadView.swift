c0d076f5ce836d43d74e2a85c4b3360123418756
Willowwryu <tuyuyou@gmail.com>
2023-03-06 16:28:11 +0900

    [Feat] UI변경점 반영
    - 전후면 버튼 제거
    - 셔터 버튼 제거

--- IAteIt/IAteIt/View/Upload/UploadView.swift
+++ IAteIt/IAteIt/View/Upload/UploadView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import AVFoundation
 
-struct UploadView: View {
+struct CameraView: View {
     @ObservedObject var viewModel = CameraViewModel()
     
     var body: some View {
@@ -19,58 +19,19 @@ struct UploadView: View {
                 }
             
             VStack {
-                HStack {
-                    Spacer()
-                    Button(action: {viewModel.switchFlash()}, label: {
-                        Image(systemName: viewModel.isFlashOn ? "bolt" : "bolt.fill")
-                            .resizable()
-                            .frame(width: 23, height: 37)
-                            .foregroundColor(.white)
-                        
-                            .padding(.trailing,20)
-                    })
-                }
                 Spacer()
                 
-                HStack {
-                    Button(action: {}, label: {
-                        ZStack{
-                            Circle()
-                                .fill(Color.red)
-                                .frame(width: 55.33, height: 55.33)
-                            Circle()
-                                .stroke(Color.white,lineWidth: 4)
-                                .frame(width: 72, height: 72)
-                        }
-                        .opacity(0)
-                        .padding(.leading)
-                    })
-                    Spacer()
-                    
-                    Button(action: {viewModel.capturePhoto()}, label: {
-                        ZStack{
-                            Circle()
-                                .fill(Color.white)
-                                .frame(width: 55.33, height: 55.33)
-                            Circle()
-                                .stroke(.white,lineWidth: 4)
-                                .frame(width: 72, height: 72)
-                        }
-                        .padding()
-                    })
-                    Spacer()
-                    
-                    Button(action: {viewModel.changeCamera()}, label: {
-                        Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
-                            .resizable()
-                            .frame(width: 48, height: 48)
-                            .foregroundColor(.white)
-                            .opacity(0.35)
-                            .padding(.trailing,20)
-                    })
-                    
-                }
-                
+                Button(action: {viewModel.capturePhoto()}, label: {
+                    ZStack{
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 55.33, height: 55.33)
+                        Circle()
+                            .stroke(.white,lineWidth: 4)
+                            .frame(width: 72, height: 72)
+                    }
+                    .padding()
+                })
             }
         }
         
@@ -111,6 +72,6 @@ struct CameraPreviewView: UIViewRepresentable {
 
 struct CameraView_Previews: PreviewProvider {
     static var previews: some View {
-        UploadView()
+        CameraView()
     }
 }
