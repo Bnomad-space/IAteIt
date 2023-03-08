1fa90c09e754fbf26aa58579efa85adcab97db24
sunshiningsoo <yenkee1219@gmail.com>
2023-03-08 19:49:17 +0900

    [Enhancement] ownerUserId -> ownerId로 변경(#6)

--- IAteIt/IAteIt/Model/Comment.swift
+++ IAteIt/IAteIt/Model/Comment.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Comment: Identifiable, Codable {
     let id: String
     let plateId: String
-    let ownerUserId: String
+    let ownerId: String
     var comment: String
     let commentTimestamp: Date
     
@@ -18,8 +18,8 @@ struct Comment: Identifiable, Codable {
 
 extension Comment {
     static let comments: [Comment] = [
-        Comment(id: UUID().uuidString, plateId: "plate1", ownerUserId: "123", comment: "This is great", commentTimestamp: Date()),
-        Comment(id: UUID().uuidString, plateId: "plate2", ownerUserId: "456", comment: "This is awful", commentTimestamp: Date())
+        Comment(id: UUID().uuidString, plateId: "plate1", ownerId: "123", comment: "This is great", commentTimestamp: Date()),
+        Comment(id: UUID().uuidString, plateId: "plate2", ownerId: "456", comment: "This is awful", commentTimestamp: Date())
         
     ]
 }
--- IAteIt/IAteIt/Model/Plate.swift
+++ IAteIt/IAteIt/Model/Plate.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Plate: Identifiable, Codable {
     let id: String
-    let ownerUserId: String
+    let ownerId: String
     var caption: String?
     var imageUrl: [String]
     var location: String?
@@ -19,8 +19,8 @@ struct Plate: Identifiable, Codable {
 
 extension Plate {
     static let plates: [Plate] = [
-        Plate(id: "plate1", ownerUserId: "123", imageUrl: ["https://cdn.shopify.com/app-store/listing_images/a78e004f44cded1b6998e7a6e081a230/promotional_image/CPKYl-_NivsCEAE=.png?height=720&width=1280"], uploadTimestamp: Date()),
-        Plate(id: "plate2", ownerUserId: "456", caption: "정말 맛있는 저녁", imageUrl: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK5q0FP74VV9wbfwP378_7kj7iDomHuKrxkXsxDdUT28V9dlVMNUe-EMzaLwaFhneeuZI&usqp=CAU"], location: "성수동", uploadTimestamp: Date())
+        Plate(id: "plate1", ownerId: "123", imageUrl: ["https://cdn.shopify.com/app-store/listing_images/a78e004f44cded1b6998e7a6e081a230/promotional_image/CPKYl-_NivsCEAE=.png?height=720&width=1280"], uploadTimestamp: Date()),
+        Plate(id: "plate2", ownerId: "456", caption: "정말 맛있는 저녁", imageUrl: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK5q0FP74VV9wbfwP378_7kj7iDomHuKrxkXsxDdUT28V9dlVMNUe-EMzaLwaFhneeuZI&usqp=CAU"], location: "성수동", uploadTimestamp: Date())
     ]
     
 }
