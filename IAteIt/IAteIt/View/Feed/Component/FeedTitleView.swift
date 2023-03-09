17ea47699b49d82290913d1d01a1422ae9d8b9c3
heokyeol <audrey2121@icloud.com>
2023-03-09 14:33:14 +0900

    FeedTitleView 주석삭제

--- IAteIt/IAteIt/View/Feed/Component/FeedTitleView.swift
+++ IAteIt/IAteIt/View/Feed/Component/FeedTitleView.swift
@@ -24,37 +24,23 @@ struct ProfilePhotoView: View {
     var profileInfo: TempFeedHeader //TODO: 프로필 데이터 수정
     
     var body: some View {
-        ZStack{
-//            HStack(alignment: .center) {
-//                Text("I ate It ")
-//                    .fontWeight(.semibold)
-//                Image(systemName: "fork.knife")
-//                    .tint(.black)
-//                    .font(.body)
-//            }
-//            HStack{
-//                Spacer()
-                Button {
-                    //TODO: 프로필 내비게이션
-                } label: {
-                    ZStack{
-                        Rectangle()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 28, height: 28)
-                        Image(profileInfo.profileImage)
-                            .resizable()
-                            .scaledToFill()
-                            .layoutPriority(-1)
-                            .frame(width: 28, height: 28)
-                    }
-                    .clipped()
-                    .cornerRadius(14)
-                    .padding([.top], 3)
-                }
-//            }
-//            .padding([.trailing], 16)
+        Button {
+            //TODO: 프로필 내비게이션
+        } label: {
+            ZStack{
+                Rectangle()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 28, height: 28)
+                Image(profileInfo.profileImage)
+                    .resizable()
+                    .scaledToFill()
+                    .layoutPriority(-1)
+                    .frame(width: 28, height: 28)
+            }
+            .clipped()
+            .cornerRadius(14)
+            .padding([.top], 3)
         }
-//        .padding([.top], 17)
-//        .padding([.bottom], 24)
     }
+    
 }
