d82d496282d359d97352278d8db8892114c16eca
sunshiningsoo <yenkee1219@gmail.com>
2023-03-22 20:08:51 +0900

    [Feat] FirebaseConnector example add(#15)

--- IAteIt/IAteIt/Network/FirebaseConnector.swift
+++ IAteIt/IAteIt/Network/FirebaseConnector.swift
@@ -11,13 +11,16 @@ import FirebaseFirestore
 
 class FirebaseConnector {
     
-    static let users = Firestore.firestore().collection("users")
     /*
-     사용 방법: TODO: - 논의된 모델로 Firebase에는 어떻게 저장할 건지 다시 바꿔주어야 함
-     FirebaseConnector.users.document("testDocument").setData(["id":"randomId"])
+     TODO: - 논의된 모델로 Firebase에는 어떻게 저장할 건지 다시 바꿔주어야 함
+     
+     사용 방법:
+     FirebaseConnector.users.document("testDocument").setData(["id":"randomId", "nickname":"jake", "profileImageUrl":"blah"])
+     
      */
     
-    
-    
+    static let users = Firestore.firestore().collection("users")
+    static let meals = Firestore.firestore().collection("meals")
+    static let comments = Firestore.firestore().collection("comments")
     
 }
