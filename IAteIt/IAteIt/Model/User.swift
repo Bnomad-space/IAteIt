35aa1a868c4f468ea241586053a1da2ba6754bd7
sunshiningsoo <yenkee1219@gmail.com>
2023-03-22 20:08:28 +0900

    [Feat] Data Model Refactor(#15)

--- IAteIt/IAteIt/Model/User.swift
+++ IAteIt/IAteIt/Model/User.swift
@@ -9,16 +9,21 @@ import Foundation
 
 struct User: Identifiable, Codable {
     let id: String
-    var userName: String
-    var profileImageUrl: String?
+    var nickname: String // Eng only, Must be unique
+    var profileImageUrl: String? // profileImageUrl nil이라면, systemName으로 Image 넣어주어야 함
     
+    init(id: String, nickname: String, profileImageUrl: String? = "default") {
+        self.id = id
+        self.nickname = nickname
+        self.profileImageUrl = profileImageUrl
+    }
     
 }
 
 extension User {
     static let users: [User] = [
-        User(id: "123", userName: "rain", profileImageUrl: "https://cdn.hswstatic.com/gif/google-update.jpg"),
-        User(id: "456", userName: "hero")
-        
+        User(id: "user1", nickname: "rain", profileImageUrl: "https://cdn.hswstatic.com/gif/google-update.jpg"),
+        User(id: "user2", nickname: "This from Hero")
     ]
+    
 }
