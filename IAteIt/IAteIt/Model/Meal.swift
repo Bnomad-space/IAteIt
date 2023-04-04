380937daaeac33efcaf91a81ba7558327adceef6
Rain <eunbkang@gmail.com>
2023-04-04 16:56:01 +0900

    [feat] #14 Added tempData

--- IAteIt/IAteIt/Model/Meal.swift
+++ IAteIt/IAteIt/Model/Meal.swift
@@ -21,7 +21,7 @@ struct Meal: Identifiable, Codable {
 extension Meal {
     static let meals = [
         Meal(id: "meal1", userId: "user1", uploadDate: Date(), plates: [Plate.plates[0], Plate.plates[1]], comments: [Comment.comments[0]]),
-        Meal(id: "meal2", userId: "user2", uploadDate: Date(), plates: [Plate.plates[2]], comments: [Comment.comments[1], Comment.comments[2]])
+        Meal(id: "meal2", userId: "user2", uploadDate: Date(), plates: [Plate.plates[2]], comments: [Comment.comments[1], Comment.comments[2]]),
+        Meal(id: "meal3", userId: "user1", location: "맥도날드", caption: "맥모닝 맛있다", uploadDate: Date(), plates: [Plate.plates[3], Plate.plates[4]], comments: [Comment.comments[3], Comment.comments[4]])
     ]
-    
 }
