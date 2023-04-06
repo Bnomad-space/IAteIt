84ef8c3250f267efad8646914df847467fb3bd41
Rain <eunbkang@gmail.com>
2023-04-06 17:23:21 +0900

    [feat] #28 username 입력 뷰 생성

--- IAteIt/IAteIt/View/SignUp/SignUpView.swift
+++ IAteIt/IAteIt/View/SignUp/SignUpView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var username = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Please create your username.")
+                .font(.headline)
+                .padding(.top, 60)
+            TextField("username", text: $username)
+                .limitTextLength($username, to: 16)
+                .textInputAutocapitalization(.never)
+                .onChange(of: username) { _ in
+                    username = username.replacingOccurrences(of: " ", with: "")
+                }
+                .font(Font.title2.weight(.bold))
+                .multilineTextAlignment(.center)
+                .padding(.top, 40)
+            Text("\(username.count) / 16")
+                .font(.caption2)
+                .foregroundColor(.gray)
+            Spacer()
+        }
+        .overlay {
+            VStack {
+            Spacer()
+                Button(action: {
+                    // TODO: username 저장, 다음 뷰로 넘기기
+                }, label: {
+                    BottomButtonView(label: "Next")
+                })
+                .disabled(username.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+            }
+        }
     }
 }
 
