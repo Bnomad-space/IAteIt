edd927eae960e3cafdf00d199e7c7e82c754648a
Rain <eunbkang@gmail.com>
2023-04-20 17:58:14 +0900

    [Feat] #36 FB functions - adding meal & plates, uploading images

--- IAteIt/IAteIt/Network/FirebaseConnector.swift
+++ IAteIt/IAteIt/Network/FirebaseConnector.swift
@@ -8,6 +8,8 @@
 import Foundation
 import Firebase
 import FirebaseFirestore
+import FirebaseStorage
+import SwiftUI
 
 class FirebaseConnector {
     
@@ -59,6 +61,87 @@ class FirebaseConnector {
             let user = User(id: id, nickname: nickname, profileImageUrl: profileImageUrl)
             
             completion(user)
+        }
+    }
+    
+    // user profile 이미지 업로드
+    func uploadProfileImage(userId: String, image: UIImage, completion: @escaping(String) -> Void) {
+        let storageRef = Storage.storage().reference()
+        let imageRef = storageRef.child("profileImage/\(userId)")
+        guard let imageData = image.jpegData(compressionQuality: 0.1) else {
+            print("DEBUG - fail compression")
+            return
+        }
+        imageRef.putData(imageData, metadata: nil) { (metadata, error) in
+            if let error = error {
+                print("DEBUG \(error.localizedDescription)")
+                return
+            }
+            imageRef.downloadURL { (url, error) in
+                if let error = error {
+                    print("DEBUG \(error.localizedDescription)")
+                    return
+                }
+                guard let imageUrl = url else { return }
+                
+                completion(imageUrl.absoluteString)
+            }
+        }
+    }
+    
+    // MARK: - meals
+    
+    // 새로운 meal 생성 (첫번째 plate 생성 포함, 캡션, 장소 없는 상태)
+    func setNewMeal(meal: Meal) {
+        let plate = meal.plates[0]
+        let plates = FirebaseConnector.meals.document(meal.id).collection("plates")
+        FirebaseConnector.meals.document(meal.id).setData([
+            "id": meal.id,
+            "userId": meal.userId,
+            "uploadDate": meal.uploadDate
+        ])
+        plates.document(plate.id).setData([
+            "id": plate.id,
+            "mealId": meal.id,
+            "imageUrl": plate.imageUrl,
+            "uploadDate": plate.uploadDate
+        ])
+    }
+    
+    // 특정 meal에 새로운 plate 추가
+    func setMealAddPlate(mealId: String, plate: Plate) {
+        let plates = FirebaseConnector.meals.document(mealId).collection("plates")
+        plates.document(plate.id).setData([
+            "id": plate.id,
+            "mealId": mealId,
+            "imageUrl": plate.imageUrl,
+            "uploadDate": plate.uploadDate
+        ])
+    }
+    
+    // plate 이미지 업로드
+    func uploadPlateImage(mealId: String, plateId: String, image: UIImage, completion: @escaping(String) -> Void) {
+        let storageRef = Storage.storage().reference()
+        let imageRef = storageRef.child("plateImage/\(plateId)")
+        let plates = FirebaseConnector.meals.document(mealId).collection("plates")
+        guard let imageData = image.jpegData(compressionQuality: 0.1) else {
+            print("DEBUG - fail compression")
+            return
+        }
+        imageRef.putData(imageData, metadata: nil) { (metadata, error) in
+            if let error = error {
+                print("DEBUG \(error.localizedDescription)")
+                return
+            }
+            imageRef.downloadURL { (url, error) in
+                if let error = error {
+                    print("DEBUG \(error.localizedDescription)")
+                    return
+                }
+                guard let imageUrl = url else { return }
+                
+                completion(imageUrl.absoluteString)
+            }
         }
     }
 }
--- IAteIt/IAteIt/Network/FirebaseTestView.swift
+++ IAteIt/IAteIt/Network/FirebaseTestView.swift
@@ -11,18 +11,30 @@ struct FirebaseTestView: View {
     
     var body: some View {
         VStack {
+            
+            // 프로필이미지 업로드, 회원가입
             Button(action: {
-                let user = User(id: "testUser1Id", nickname: "testUser1Nickname")
-                FirebaseConnector().setNewUser(user: user)
+                var user = User(id: "testUser1Id", nickname: "testUser1Nickname")
+                let image = UIImage(named: "Sample_Profile1")
+                if let image = image {
+                    FirebaseConnector().uploadProfileImage(userId: user.id, image: image) { url in
+                        user.profileImageUrl = url
+                        FirebaseConnector().setNewUser(user: user)
+                    }
+                }
             }, label: {
                 Text("Sign up")
             })
+            
+            // 프로필 정보 수정
             Button(action: {
                 let user = User(id: "testUser1Id", nickname: "testUser1NicknameEdited", profileImageUrl: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80")
                 FirebaseConnector().updateUser(user: user)
             }, label: {
                 Text("Edit User")
             })
+            
+            // 특정 user 정보 가져오기
             Button(action: {
                 FirebaseConnector().fetchUser(id: "testUser1Id") { user in
                     print(user.id)
@@ -34,6 +46,38 @@ struct FirebaseTestView: View {
             }, label: {
                 Text("Fetch user")
             })
+            
+            // 새로운 meal 생성 & plate 이미지 업로드
+            Button(action: {
+                let image = UIImage(named: "Sample_McMorning")
+                var meal = Meal(id: "testMealId1", userId: "testUser1Id", uploadDate: Date(), plates: [])
+                var plate = Plate(id: "testPlateId1", mealId: meal.id, imageUrl: "", uploadDate: Date())
+                if let image = image {
+                    FirebaseConnector().uploadPlateImage(mealId: meal.id, plateId: plate.id, image: image) { url in
+                        plate.imageUrl = url
+                        meal.plates.append(plate)
+                        FirebaseConnector().setNewMeal(meal: meal)
+                    }
+                }
+            }, label: {
+                Text("Create a meal")
+            })
+            
+            // meal에 plate 추가
+            Button(action: {
+                let image = UIImage(named: "Sample_McMorning")
+                var plate2 = Plate(id: "testPlateId2", mealId: "testMealId1", imageUrl: "", uploadDate: Date())
+                if let image = image {
+                    FirebaseConnector().uploadPlateImage(mealId: plate2.mealId, plateId: plate2.id, image: image) { url in
+                        plate2.imageUrl = url
+                        FirebaseConnector().setMealAddPlate(mealId: plate2.mealId, plate: plate2)
+                    }
+                }
+            }, label: {
+                Text("Add a plate to a meal")
+            })
+            
+            
         }
     }
 }
