d1ea02ea2d9ad00ee213342c4eb7ec28e2e4c5ba
Rain <eunbkang@gmail.com>
2023-04-25 23:43:52 +0900

    [Feat] #38 Apple 로그인과 Firebase 연결
    Sign in with Apple 하면 Firebase Authentication users에 등록

--- IAteIt/IAteIt/View/SignUp/LoginView.swift
+++ IAteIt/IAteIt/View/SignUp/LoginView.swift
@@ -5,10 +5,14 @@
 //  Created by Eunbee Kang on 2023/03/26.
 //
 
-import SwiftUI
 import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
+import SwiftUI
 
 struct LoginView: View {
+    @State var currentNonce: String?
+    
     var body: some View {
         VStack {
             Text("Sign in required!")
@@ -21,22 +25,62 @@ struct LoginView: View {
                 .padding([.bottom], 44)
             
             SignInWithAppleButton(.signIn) { request in
-                  request.requestedScopes = [.fullName, .email]
-                } onCompletion: { result in
-                  switch result {
-                    case .success(let authResults):
-                      print("Authorisation successful \(authResults)")
-                    case .failure(let error):
-                      print("Authorisation failed: \(error.localizedDescription)")
-                  }
-                }
-                .signInWithAppleButtonStyle(.white)
-                .frame(width: 268, height: 50, alignment: .center)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(.black, lineWidth: 1)
-                )
+                let nonce = randomNonceString()
+                currentNonce = nonce
+                request.requestedScopes = [.fullName, .email]
+                request.nonce = sha256(nonce)
+                
+            } onCompletion: { result in
+                switch result {
+                case .success(let authResults):
+                    print("Authorisation successful \(authResults)")
+                  
+                    switch authResults.credential {
+                    case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                        guard let nonce = currentNonce else {
+                            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                        }
+                        guard let appleIDToken = appleIDCredential.identityToken else {
+                            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                        }
+                        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                            return
+                        }
+                        let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+                        Auth.auth().signIn(with: credential) { (authResult, error) in
+                            if error != nil {
+                                // Error. If error.code == .MissingOrInvalidNonce, make sure
+                                // you're sending the SHA256-hashed nonce as a hex string with
+                                // your request to Apple.
+                                print(error?.localizedDescription as Any)
+                                return
+                            }
+                            if let user = authResult?.user {
+                                print("애플 로그인 결과:", user.uid, user.email ?? "-")
+                            }
+                            print("signed in")
+                        }
+                        print("\(String(describing: Auth.auth().currentUser?.uid))")
+                        
+                    default:
+                        break
+                    }
+                    
+                case .failure(let error):
+                  print("Authorisation failed: \(error.localizedDescription)")
+              }
+            }
+            .signInWithAppleButtonStyle(.white)
+            .frame(width: 268, height: 50, alignment: .center)
+            .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(.black, lineWidth: 1)
+            )
                 .padding([.bottom], 32)
+            
+            
+            
             
             Button(action: {
                 // TODO: 액션 추가
@@ -48,6 +92,45 @@ struct LoginView: View {
             })
         }
     }
+}
+
+extension LoginView {
+    
+    
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+    
+    // from https://firebase.google.com/docs/auth/ios/apple
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      var randomBytes = [UInt8](repeating: 0, count: length)
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+      if errorCode != errSecSuccess {
+        fatalError(
+          "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+        )
+      }
+
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+
+      let nonce = randomBytes.map { byte in
+        // Pick a random character from the set, wrapping around if needed.
+        charset[Int(byte) % charset.count]
+      }
+
+      return String(nonce)
+    }
+    
+    
 }
 
 struct LoginView_Previews: PreviewProvider {
