514ffe7dd1b0a2663d4f6c87be11b58121e77ce5
sunshiningsoo <yenkee1219@gmail.com>
2023-05-24 19:55:05 +0900

    [feat] comment meal 의존성 삭제

--- IAteIt/IAteIt/View/Feed/Model/FeedMealModel.swift
+++ IAteIt/IAteIt/View/Feed/Model/FeedMealModel.swift
@@ -15,6 +15,7 @@ final class FeedMealModel: ObservableObject {
         }
     }
     @Published var allUsers: [User] = []
+    @Published var commentList: [String: [Comment]] = [:]
     
     init() {
         Task {
@@ -29,14 +30,10 @@ final class FeedMealModel: ObservableObject {
     @MainActor
     func getMealListIn24Hours() {
         Task {
-            var localMealList = try await FirebaseConnector.shared.fetchMealIn24Hours(date: Date())
-            var tempList: [Meal] = []
-            for meal in localMealList {
+            self.mealList = try await FirebaseConnector.shared.fetchMealIn24Hours(date: Date())
+            for meal in self.mealList {
                 FirebaseConnector.shared.fetchMealComments(mealId: meal.id!) { comments in
-                    var temp = meal
-                    temp.comments = comments
-                    tempList.append(temp)
-                    self.mealList = tempList.sorted { $0.uploadDate > $1.uploadDate }
+                    self.commentList[meal.id!] = comments
                 }
             }
         }
@@ -47,12 +44,8 @@ final class FeedMealModel: ObservableObject {
             if let current = Auth.auth().currentUser {
                 let uploadComment = Comment(id: UUID().uuidString, userId: current.uid, mealId: meal.id!, comment: comment, uploadDate: Date())
                 await FirebaseConnector.shared.setNewComment(comment: uploadComment)
-                mealList.indices.forEach { index in
-                    if mealList[index].id == meal.id {
-                        DispatchQueue.main.async {
-                            self.mealList[index].comments?.append(uploadComment)
-                        }
-                    }
+                DispatchQueue.main.async {
+                    self.commentList[meal.id!]!.append(uploadComment)
                 }
             }
         }
