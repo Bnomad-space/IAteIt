3d59948530467ef9d728f36e4d7ce917006c7db4
heokyeol <audrey2121@icloud.com>
2023-07-21 18:56:01 +0900

    reportView UI 피그마로 수정

--- IAteIt/IAteIt/View/MealDetail/Component/ReportView.swift
+++ IAteIt/IAteIt/View/MealDetail/Component/ReportView.swift
@@ -8,36 +8,82 @@
 import SwiftUI
 
 struct ReportView: View {
+    let photoCorner: CGFloat = 20
+    let iconSize: CGFloat = 48
     var meal: Meal
+    var user: User
     var reason = ""
     @EnvironmentObject var loginState: LoginStateModel
     @Binding var isReportPresented: Bool
+    @State private var isAlertPresented: Bool = false
     @State private var text: String = ""
 
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    isReportPresented = false
-                }, label: {
-                    Text("취소")
-                        .foregroundColor(.blue)
-                })
-                .padding()
+        NavigationView{
+            VStack {
+                
+                Text("\(user.nickname)'s meal")
+                    .fontWeight(.semibold)
+                    .padding(20)
+                
+                ZStack {
+                    Rectangle()
+                        .aspectRatio(1, contentMode: .fit)
+                        .cornerRadius(photoCorner)
+                        .foregroundColor(.white)
+                    
+                    if let url = URL(string: meal.plates[0].imageUrl) {
+                        CacheAsyncImage(url: url) { phase in
+                            switch phase {
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .layoutPriority(-1)
+                                    .cornerRadius(photoCorner)
+                            case .failure(_):
+                                PlateImageErrorView(iconSize: iconSize)
+                            case .empty:
+                                Color(UIColor.systemGray6)
+                            @unknown default:
+                                PlateImageErrorView(iconSize: iconSize)
+                            }
+                        }
+                    }
+                }
+                .frame(height: 100)
+                .clipped()
+                .cornerRadius(photoCorner)
+                
+                TextField("Please tell us why you are reporting this meal.", text: $text)
+                    .frame(height: 100)
+                    .textFieldStyle(.plain)
+                    .padding(.horizontal)
                 Spacer()
             }
-            Spacer()
-            Text("신고하겠습니다")
-            TextField("Enter text", text: $text)
-                            .frame(height: 100)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-            Button(action: {
-                FirebaseConnector().sendReport(reporterId: loginState.user?.id ?? "", reportedTime: Date(), mealId: meal.id ?? "", reason: text)
-                isReportPresented = false
-            }, label: {
-                Label("신고하기", systemImage: "trash")
+            .navigationBarTitle("Report", displayMode: .inline)
+            .navigationBarItems(leading: Button(action: {
+                                        isReportPresented = false
+                                    }, label: {
+                                        Image(systemName: "xmark")
+                                            .foregroundColor(.black)
+                                    }),
+                                    trailing:
+                                        Button(action: {
+                                            FirebaseConnector().sendReport(reporterId: loginState.user?.id ?? "", reportedTime: Date(), mealId: meal.id ?? "", reason: text)
+                                            isAlertPresented = true
+                                        }, label: {
+                                            Image(systemName: "paperplane.fill")
+                                                .foregroundColor(.black)
+                                        })
+                                )
+            .alert("Report Completed", isPresented: $isAlertPresented, actions: {
+                Button("OK", role: .cancel, action: {
+                    isReportPresented = false
+                })
+            }, message: {
+                Text("Your report has been submitted successfully.")
             })
-            Spacer()
         }
     }
 }
