fedeee5030abab97920e13603f8865662553f2bf
Rain <eunbkang@gmail.com>
2023-09-06 00:48:52 +0900

    [fix] set different var for different webViews

--- IAteIt/IAteIt/View/Setting/SettingView.swift
+++ IAteIt/IAteIt/View/Setting/SettingView.swift
@@ -11,7 +11,8 @@ struct SettingView: View {
     @EnvironmentObject var loginState: LoginStateModel
     @EnvironmentObject var feedMeals: FeedMealModel
     @State private var isShowingDeleteAccountAlert = false
-    @State private var isPresentWebView = false
+    @State private var isPresentTermsOfUseWebView = false
+    @State private var isPresentPrivacyPolicyWebView = false
     @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
     @Environment(\.rootPresentationMode) private var rootPresentationMode: Binding<RootPresentationMode>
     
@@ -33,11 +34,11 @@ struct SettingView: View {
             }
             Section(header: Text("Information")) {
                 Button(action: {
-                    isPresentWebView = true
+                    isPresentTermsOfUseWebView = true
                 }, label: {
                     SettingListTitleView(text: "Terms of Use", symbol: "doc.text", color: .black)
                 })
-                .sheet(isPresented: $isPresentWebView) {
+                .sheet(isPresented: $isPresentTermsOfUseWebView) {
                     NavigationView {
                         WebView(url: URL(string: Const.URL.termsOfUse.rawValue)!)
                             .navigationTitle("Terms of Use")
@@ -45,11 +46,11 @@ struct SettingView: View {
                     }
                 }
                 Button(action: {
-                    isPresentWebView = true
+                    isPresentPrivacyPolicyWebView = true
                 }, label: {
                     SettingListTitleView(text: "Privacy Policy", symbol: "lock.doc", color: .black)
                 })
-                .sheet(isPresented: $isPresentWebView) {
+                .sheet(isPresented: $isPresentPrivacyPolicyWebView) {
                     NavigationView {
                         WebView(url: URL(string: Const.URL.privacyPolicy.rawValue)!)
                             .navigationTitle("Privacy Policy")
