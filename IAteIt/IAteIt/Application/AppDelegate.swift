05c34c58bc0fa5bfabd8daa5eee2a9471d1a8262
eunbkang <eunbkang@gmail.com>
2024-06-06 01:26:19 +0900

    [Feature] #140 로컬 알림 추가

--- IAteIt/IAteIt/Application/AppDelegate.swift
+++ IAteIt/IAteIt/Application/AppDelegate.swift
@@ -13,7 +13,45 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
       FirebaseApp.configure()
       let _ = Firestore.firestore()
+      
+      UNUserNotificationCenter.current().delegate = self
+      UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { isAuthorized, error in
+          if let error {
+              print("[requestAuthorization error]:", error.localizedDescription)
+          }
+          if isAuthorized {
+              self.setLocalNotifications()
+          }
+      }
+      
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+          if settings.authorizationStatus == .authorized {
+              self.setLocalNotifications()
+          } else {
+              UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+          }
+      }
+      
       return true
   }
     
+    private func setLocalNotifications() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        [Weekday.monday, Weekday.wednesday, Weekday.friday].forEach { weekday in
+            notificationCenter.addLocalNotificationRequest(hour: 19, day: weekday)
+        }
+        
+        [Weekday.tuesday, Weekday.thursday, Weekday.saturday].forEach { weekday in
+            notificationCenter.addLocalNotificationRequest(hour: 12, day: weekday)
+        }
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .sound])
+    }
 }
